2801cc926cbf14a4f9ac5101d3a4db81c2d95e6d
leyfon18 <crj18051992@gmail.com>
2015-04-21 18:37:20 +0200

    Score modify - no more switches - some if in cascade to fix Test running good

--- tennis-kata/src/main/java/TennisGame1.java
+++ tennis-kata/src/main/java/TennisGame1.java
@@ -1,4 +1,3 @@
-
 public class TennisGame1 implements TennisGame {
 
 	private int player1Score = 0;
@@ -6,7 +5,7 @@ public class TennisGame1 implements TennisGame {
 	private String player1Name;
 	private String player2Name;
 	private String[] scoreName = { "Love", "Fifteen", "Thirty", "Forty",
-			"Advantage" };
+			"Deuce" };
 
 	public TennisGame1(String player1Name, String player2Name) {
 		this.player1Name = player1Name;
@@ -23,64 +22,61 @@ public class TennisGame1 implements TennisGame {
 	public String getScore() {
 		String score = "";
 		int tempScore = 0;
-		if (isPair()) {
-			switch (player1Score) {
-			case 0:
-				score = "Love-All";
-				break;
-			case 1:
-				score = "Fifteen-All";
-				break;
-			case 2:
-				score = "Thirty-All";
-				break;
-			default:
-				score = "Deuce";
-				break;
-
-			}
-		} else if (isScoreMore4()) {
-			int minusResult = player1Score - player2Score;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
+		if (!isScoreMore4()) {
+			score += getScoreName(player1Score);
+			score += "-";
+			if (isPair())
+				if (isDeuceWhenPair())
+					score = "Deuce";
+				else
+					score += "All";
 			else
-				score = "Win for player2";
+				score += getScoreName(player2Score);
 		} else {
-			// that iteration is useless
-			  for (int i=1; i<3; i++)
-	            {
-	                if (i==1) tempScore = player1Score;
-	                else { score+="-"; tempScore = player2Score;}
-	                score += getScoreName( tempScore);
-	            }
+			if (isScoreMore4()) {
+				if (isPair())
+					score = "Deuce";
+				else {
+					int minusResult = player1Score - player2Score;
+					if (minusResult == 1)
+						score = "Advantage player1";
+					else if (minusResult == -1)
+						score = "Advantage player2";
+					else if (minusResult >= 2)
+						score = "Win for player1";
+					else
+						score = "Win for player2";
+
+				}
 			}
+		}
 		return score;
 	}
 
-	private String getScoreName( int tempScore) {
-//		switch(tempScore)
-//		{
-//		    case 0:
-//		        score+="Love";
-//		        break;
-//		    case 1:
-//		        score+="Fifteen";
-//		        break;
-//		    case 2:
-//		        score+="Thirty";
-//		        break;
-//		    case 3:
-//		        score+="Forty";
-//		        break;
-//		}
-//		return score;
+	private boolean isDeuceWhenPair() {
+		return player2Score == 3;
+	}
+
+	private String getScoreName(int tempScore) {
+		// switch(tempScore)
+		// {
+		// case 0:
+		// score+="Love";
+		// break;
+		// case 1:
+		// score+="Fifteen";
+		// break;
+		// case 2:
+		// score+="Thirty";
+		// break;
+		// case 3:
+		// score+="Forty";
+		// break;
+		// }
+		// return score;
+
 		return scoreName[tempScore];
-		
-		
+
 	}
 
 	private boolean isScoreMore4() {
