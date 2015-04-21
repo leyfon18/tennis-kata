4766d064ca4bb0774a8702591826fb67480c41cf
leyfon18 <crj18051992@gmail.com>
2015-04-21 17:00:09 +0200

    First Commit; becouse error in creating git repository

---
+++ tennis-kata/src/main/java/TennisGame1.java
@@ -0,0 +1,93 @@
+
+public class TennisGame1 implements TennisGame {
+
+	private int player1Score = 0;
+	private int player2Score = 0;
+	private String player1Name;
+	private String player2Name;
+	private String[] scoreName = { "Love", "Fifteen", "Thirty", "Forty",
+			"Advantage" };
+
+	public TennisGame1(String player1Name, String player2Name) {
+		this.player1Name = player1Name;
+		this.player2Name = player2Name;
+	}
+
+	public void wonPoint(String playerName) {
+		if (playerName == player1Name)
+			player1Score++;
+		else
+			player2Score++;
+	}
+
+	public String getScore() {
+		String score = "";
+		int tempScore = 0;
+		if (isPair()) {
+			switch (player1Score) {
+			case 0:
+				score = "Love-All";
+				break;
+			case 1:
+				score = "Fifteen-All";
+				break;
+			case 2:
+				score = "Thirty-All";
+				break;
+			default:
+				score = "Deuce";
+				break;
+
+			}
+		} else if (isScoreMore4()) {
+			int minusResult = player1Score - player2Score;
+			if (minusResult == 1)
+				score = "Advantage player1";
+			else if (minusResult == -1)
+				score = "Advantage player2";
+			else if (minusResult >= 2)
+				score = "Win for player1";
+			else
+				score = "Win for player2";
+		} else {
+			// that iteration is useless
+			  for (int i=1; i<3; i++)
+	            {
+	                if (i==1) tempScore = player1Score;
+	                else { score+="-"; tempScore = player2Score;}
+	                score += getScoreName( tempScore);
+	            }
+			}
+		return score;
+	}
+
+	private String getScoreName( int tempScore) {
+//		switch(tempScore)
+//		{
+//		    case 0:
+//		        score+="Love";
+//		        break;
+//		    case 1:
+//		        score+="Fifteen";
+//		        break;
+//		    case 2:
+//		        score+="Thirty";
+//		        break;
+//		    case 3:
+//		        score+="Forty";
+//		        break;
+//		}
+//		return score;
+		return scoreName[tempScore];
+		
+		
+	}
+
+	private boolean isScoreMore4() {
+		return player1Score >= 4 || player2Score >= 4;
+	}
+
+	private boolean isPair() {
+		return player1Score - player2Score == 0;
+	}
+}
